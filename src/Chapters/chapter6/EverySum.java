765702f384ef36bf00f635cf6d3155a4bb1716ec
Enoch <enoch@Jeds-Macbook-Pro.local>
2018-01-22 11:52:35 -0700

    added EverySum Program

---
+++ src/Chapters/chapter6/EverySum.java
@@ -0,0 +1,15 @@
+package Chapters.chapter6;
+
+import javax.swing.JOptionPane;
+
+public class EverySum {
+	public static void main(String[] args) {
+		int sum = 0;
+		String input = JOptionPane.showInputDialog("Please enter an integer between 1 and 50 to find the sum of all numbers from 1 to the entered integer");
+		int num = Integer.parseInt(input);
+		for (int i = 0; i <= num; i++) {
+			sum += i;
+		}
+		System.out.println("The sum of all numbers from 1 to " + input + " is: " + sum);
+	}
+}
