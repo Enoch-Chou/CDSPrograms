f00f963681eb8c5c0a61c26a4304e8fa1fad20a2
Enoch <enoch@Jeds-Macbook-Pro.local>
2018-01-23 12:35:22 -0700

    updated FactorGenerator program

--- src/Chapters/chapter6/FactorGeneratorTest.java
+++ src/Chapters/chapter6/FactorGeneratorTest.java
@@ -4,12 +4,22 @@ import javax.swing.JOptionPane;
 
 public class FactorGeneratorTest {
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("Please enter a digit to prime factorize");
-		int num = Integer.parseInt(input);
-		FactorGenerator hi = new FactorGenerator(num);
-		System.out.print("Prime Factorization of " + input + ": ");
-		while (hi.hasNextFactor()) {
-			hi.nextFactor();
+		boolean flag = true;
+		while (flag) {
+			String input = JOptionPane.showInputDialog("Please enter a digit to prime factorize, press cancel to cancel the program");
+			if (input == null) {
+				flag = false;
+				System.out.println("Congratulations, your program has ended");
+			}
+			else {
+			int num = Integer.parseInt(input);
+			FactorGenerator hi = new FactorGenerator(num);
+			System.out.print("Prime Factorization of " + input + ": ");
+			while (hi.hasNextFactor()) {
+				hi.nextFactor();
+				}
+			System.out.println();
+			}
 		}
 	}
 }
