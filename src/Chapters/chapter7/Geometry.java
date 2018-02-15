da109577f4e3b44ceb2cc715a1d7f75a63eed871
Enoch <enoch@Jeds-Macbook-Pro.local>
2018-02-15 12:54:50 -0700

    added pointDistance

--- src/Chapters/chapter7/Geometry.java
+++ src/Chapters/chapter7/Geometry.java
@@ -19,4 +19,7 @@ public class Geometry {
 	public static double coneSurface(double r, double h) {
 		return Math.PI * r * (h + r);
 	}
+	public static double pointDistance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow((y2 - y1),2) + Math.pow((x2-x1), 2));
+	}
 }
--- src/Chapters/chapter7/GeometryTest.java
+++ src/Chapters/chapter7/GeometryTest.java
@@ -1,9 +1,14 @@
 package Chapters.chapter7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 import javax.swing.JOptionPane;
 
 public class GeometryTest {
-		public static void main(String[] args) {
+		public static void main(String[] args) throws FileNotFoundException {
 			String radius = JOptionPane.showInputDialog("Enter in the length of the radius");
 			double r = Double.parseDouble(radius);
 			String height = JOptionPane.showInputDialog("Enter in the length of height");
@@ -17,7 +22,21 @@ public class GeometryTest {
 			System.out.printf("%s%5.2f%n", "The surface area of the cylinder is: ", hi.cylinderSurface(r, h));
 			System.out.printf("%s%5.2f%n", "The volume of a cone is: ", hi.coneVolume(r, h));
 			System.out.printf("%s%5.2f%n", "The surface area of a cone is: ", hi.coneSurface(r, h));
-
+			FileReader reader = new FileReader("src/Chapters/chapter7/points");
+			Scanner file = new Scanner(reader);
+			int i = 0;
+			while (i < 3) {
+				String input = file.nextLine();
+				StringTokenizer tokenizer = new StringTokenizer(input);
+				i++;
+				while (tokenizer.hasMoreTokens()) {
+					double x1 = Double.parseDouble(tokenizer.nextToken());
+					double y1 = Double.parseDouble(tokenizer.nextToken());
+					double x2 = Double.parseDouble(tokenizer.nextToken());
+					double y2 = Double.parseDouble(tokenizer.nextToken());
+					System.out.printf("%s%2.2f%s%2.2f%s%2.2f%s%2.2f%s%2.2f%n", "The distance between (",x1, ",", y1, ") and (", x2, ",", y2, ") = ",hi.pointDistance(x1, y1, x2, y2));
+				}
+			}
 	}
 }
 	
